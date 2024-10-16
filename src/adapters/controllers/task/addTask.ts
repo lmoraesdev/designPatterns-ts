0f9c9060fe7f17c009c13063a8ccbb5dce249be0
lmoraesdev <lbc_moraes@outlook.com>
2024-10-16 01:06:16 -0300

    refactor: estudo de desacoplar controller  do appcontroller

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -1,33 +1,34 @@
-import validator from "validator";
-import { HttpRequest, HttpResponse } from "../../interfaces/http";
-import { InvalidParamError } from "../../presentations/api/errors/invalid-param-error";
-import { MissingParamError } from "../../presentations/api/errors/missing-param-error";
+import validator from 'validator';
+import { HttpRequest, HttpResponse } from '../../interfaces/http';
+import { InvalidParamError } from '../../presentations/api/errors/invalid-param-error';
+import { MissingParamError } from '../../presentations/api/errors/missing-param-error';
 import {
   badRequest,
   created,
-} from "../../presentations/api/httpResponses/httpResponses";
-import { Request, Response } from "express";
+} from '../../presentations/api/httpResponses/httpResponses';
+import { Request, Response } from 'express';
+import { Controller } from '../../interfaces/controller';
 
-export class AddTaskController {
-  async handle(req: Request, res: Response) {
-    const requiredFields = ["title", "description", "date"];
+export class AddTaskController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['title', 'description', 'date'];
 
     for (const field of requiredFields) {
-      if (!req.body[field]) {
+      if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
     }
-    const { title, description, date } = req.body;
+    const { title, description, date } = httpRequest.body;
 
     const isValid = validator.isDate(date, {
-      format: "DD-MM-YYYY",
+      format: 'DD-MM-YYYY',
     });
 
     if (!isValid) {
-      return badRequest(new InvalidParamError("date"));
+      return badRequest(new InvalidParamError('date'));
     }
 
     const task = { title, description, date };
-    res.json(created(task));
+    return created(task);
   }
 }
