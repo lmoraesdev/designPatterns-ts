db1bd84ea2a5d484e7daea3dcf14933dceffa7ec
lmoraesdev <lbc_moraes@outlook.com>
2024-10-17 11:08:46 -0300

    refactor: criacao do adaptador de validador para desacoplar dateValidator

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -1,17 +1,19 @@
-import validator from 'validator';
-import { HttpRequest, HttpResponse } from '../../interfaces/http';
-import { InvalidParamError } from '../../presentations/api/errors/invalid-param-error';
-import { MissingParamError } from '../../presentations/api/errors/missing-param-error';
+import validator from "validator";
+import { HttpRequest, HttpResponse } from "../../interfaces/http";
+import { InvalidParamError } from "../../presentations/api/errors/invalid-param-error";
+import { MissingParamError } from "../../presentations/api/errors/missing-param-error";
 import {
   badRequest,
   created,
-} from '../../presentations/api/httpResponses/httpResponses';
-import { Request, Response } from 'express';
-import { Controller } from '../../interfaces/controller';
+} from "../../presentations/api/httpResponses/httpResponses";
+import { Request, Response } from "express";
+import { Controller } from "../../interfaces/controller";
+import { DateValidator } from "../../interfaces/dateValidator";
 
 export class AddTaskController implements Controller {
+  constructor(private readonly dateValidator: DateValidator) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['title', 'description', 'date'];
+    const requiredFields = ["title", "description", "date"];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
@@ -20,12 +22,10 @@ export class AddTaskController implements Controller {
     }
     const { title, description, date } = httpRequest.body;
 
-    const isValid = validator.isDate(date, {
-      format: 'DD-MM-YYYY',
-    });
+    const isValid = this.dateValidator.isValid(date);
 
     if (!isValid) {
-      return badRequest(new InvalidParamError('date'));
+      return badRequest(new InvalidParamError("date"));
     }
 
     const task = { title, description, date };
